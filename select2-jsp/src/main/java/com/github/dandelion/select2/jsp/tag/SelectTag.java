1002819ac96b40b3d627dd75eabe6b4ae6001b39
Thibault Duchateau <thibault.duchateau@gmail.com>
2015-07-26 23:47:52 +0200

    feat(jsp): first tests with a JSP taglib

--- select2-jsp/src/main/java/com/github/dandelion/select2/jsp/tag/SelectTag.java
+++ select2-jsp/src/main/java/com/github/dandelion/select2/jsp/tag/SelectTag.java
@@ -32,7 +32,9 @@ package com.github.dandelion.select2.jsp.tag;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -44,159 +46,237 @@ import org.apache.commons.beanutils.PropertyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.dandelion.core.asset.generator.JavascriptGenerator;
-import com.github.dandelion.core.asset.generator.jquery.StandardJQueryJavascriptGenerator;
-import com.github.dandelion.core.asset.locator.impl.DelegateLocator;
+import com.github.dandelion.core.asset.generator.js.jquery.JQueryContent;
+import com.github.dandelion.core.asset.generator.js.jquery.JQueryJsContentGenerator;
+import com.github.dandelion.core.option.Option;
+import com.github.dandelion.core.util.OptionUtils;
 import com.github.dandelion.core.web.AssetRequestContext;
-import com.github.dandelion.select2.core.generator.Select2AssetBuffer;
-import com.github.dandelion.select2.core.generator.jquery.Select2JQueryJavascriptGenerator;
+import com.github.dandelion.select2.core.Select2Component;
+import com.github.dandelion.select2.core.generator.Select2JQueryContent;
 import com.github.dandelion.select2.core.html.HtmlOption;
 import com.github.dandelion.select2.core.html.HtmlSelect;
+import com.github.dandelion.select2.core.option.Select2Options;
 
+/**
+ * <p>
+ * JSP tag used for creating HTML select.
+ * </p>
+ * 
+ * @author Thibault Duchateau
+ * @since 1.0.0
+ */
 public class SelectTag extends TagSupport implements DynamicAttributes {
 
-	private static final long serialVersionUID = 1528832829817373708L;
+   private static final long serialVersionUID = 1528832829817373708L;
 
-	// Logger
-	protected static Logger logger = LoggerFactory.getLogger(SelectTag.class);
+   protected static Logger logger = LoggerFactory.getLogger(SelectTag.class);
 
-	/**
-	 * Tag attributes
-	 */
-	protected String id;
-	protected String itemValue;
-	protected String itemLabel;
+   protected String id;
+   protected String itemValue;
+   protected String itemLabel;
 
-	// First way to populate the select: using a Collection
-	protected Object data;
+   protected Object data;
 
-	// Second way to populate the select: using the URL that returns data
-	// protected String url;
+   /**
+    * Map containing the staging options to be applied to the select at the end
+    * of the tag processing.
+    */
+   private Map<Option<?>, Object> stagingOptions;
 
-	/**
-	 * Internal attributes
-	 */
-	protected HtmlSelect select;
-	protected String dataSourceType;
-	protected Iterator<Object> iterator;
-	protected Object currentObject;
-	protected HttpServletRequest request;
-	protected HttpServletResponse response;
+   /**
+    * The map of dynamic attributes that will be set as-is on the select tag.
+    */
+   protected Map<String, String> dynamicAttributes;
 
-	@Override
-	public void setDynamicAttribute(String uri, String localName, Object value) throws JspException {
-		// TODO Auto-generated method stub
+   /**
+    * The select tag to be built.
+    */
+   protected HtmlSelect select;
 
-	}
+   protected Object currentObject;
 
-	@Override
-	public int doStartTag() throws JspException {
-		
-		request = (HttpServletRequest) pageContext.getRequest();
-		response = (HttpServletResponse) pageContext.getResponse();
-		
-		select = new HtmlSelect(id, request, response, null, null);
-		
-		try {
-			while (iterator.hasNext()) {
-				Object o = iterator.next();
-				Object propertyValue = PropertyUtils.getNestedProperty(o, itemValue);
-				HtmlOption option = new HtmlOption();
-				option.setText(String.valueOf(propertyValue));
-				option.setValue(String.valueOf(propertyValue));
-				select.addOption(option);
-			}
-		}
-		catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return super.doStartTag();
-	}
+   /**
+    * The iterator to be used to feed the table with the provided DOM source.
+    */
+   private Iterator<Object> iterator;
 
-	@Override
-	public int doEndTag() throws JspException {
-		return setupHtmlGeneration();
-	}
+   /**
+    * The type of data source configured in the tag. Depending on this type, the
+    * tag won't behaviour in the same way.
+    */
+   private String dataSourceType;
 
-	protected int setupHtmlGeneration() throws JspException {
+   /**
+    * The current request.
+    */
+   protected HttpServletRequest request;
 
-		// Get the right Javascript generator or create it if it doesn't exist
-		JavascriptGenerator javascriptGenerator = AssetRequestContext.get(this.request).getParameterValue(
-				"dandelion-select2", DelegateLocator.DELEGATED_CONTENT_PARAM);
-		
-		if (javascriptGenerator == null) {
-			javascriptGenerator = new StandardJQueryJavascriptGenerator();
-		}
-		
-		// Generate the web resources (JS, CSS) and wrap them into a
-		// WebResources POJO
-		Select2AssetBuffer dab = Select2AssetBuffer.create(this.select);
-		logger.debug("Web content generated successfully");
+   /**
+    * The current response.
+    */
+   protected HttpServletResponse response;
 
-		// Buffering generated Javascript
-		javascriptGenerator.fillBuffer(dab);
+   public SelectTag() {
+      this.stagingOptions = new HashMap<Option<?>, Object>();
+   }
 
-		// Update the asset request context with the enabled bundles and
-				// Javascript generator
-		AssetRequestContext
-			.get(this.request)
-			.addBundles("ddl-s2")
-			.addBundles("select2")
-			.addParameter("dandelion-select2", DelegateLocator.DELEGATED_CONTENT_PARAM, javascriptGenerator,
-					false);
+   @Override
+   public int doStartTag() throws JspException {
 
-		try {
-			this.pageContext.getOut().println(this.select.toHtml());
+      request = (HttpServletRequest) pageContext.getRequest();
+      response = (HttpServletResponse) pageContext.getResponse();
 
-		}
-		catch (IOException e) {
-			throw new JspException("Unable to generate the HTML markup for the table " + id, e);
-		}
+      select = new HtmlSelect(id, request, response, null, dynamicAttributes);
+      // HtmlOption op = new HtmlOption();
+      // op.setValue("");
+      // select.addOption(op);
+      try {
+         while (iterator.hasNext()) {
+            Object o = iterator.next();
+            Object propertyValue = PropertyUtils.getNestedProperty(o, itemValue);
+            HtmlOption option = new HtmlOption();
+            option.setText(String.valueOf(propertyValue));
+            option.setValue(String.valueOf(propertyValue));
+            select.addOption(option);
+         }
+      }
+      catch (IllegalAccessException e) {
+         throw new JspException(e);
+      }
+      catch (InvocationTargetException e) {
+         throw new JspException(e);
+      }
+      catch (NoSuchMethodException e) {
+         throw new JspException(e);
+      }
+      return super.doStartTag();
+   }
 
-		return EVAL_PAGE;
-	}
+   @Override
+   public int doEndTag() throws JspException {
 
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public void setData(Collection<Object> data) {
-		this.dataSourceType = "DOM";
-		this.data = data;
+      // At this point, all setters have been called and the staging
+      // configuration map should have been filled with user configuration
+      // The user configuration can now be applied to the default
+      // configuration
+      this.select.getSelectConfiguration().getOptions().putAll(this.stagingOptions);
 
-		Collection<Object> dataTmp = (Collection<Object>) data;
-		if (dataTmp != null && dataTmp.size() > 0) {
-			iterator = dataTmp.iterator();
-		}
-		else {
-			iterator = null;
-			currentObject = null;
-		}
-	}
+      // Once all configuration are merged, they can be processed
+      OptionUtils.processOptions(this.select.getSelectConfiguration().getOptions(), request);
 
-	public void setItemValue(String itemValue) {
-		this.itemValue = itemValue;
-	}
+      return setupHtmlGeneration();
+   }
 
-	public void setItemLabel(String itemLabel) {
-		this.itemLabel = itemLabel;
-	}
+   @Override
+   public void setDynamicAttribute(String uri, String localName, Object value) throws JspException {
+      validateDynamicAttribute(localName, value);
 
-	// public void setUrl(String url) {
-	// String processedUrl = UrlUtils.getProcessedUrl(url, (HttpServletRequest)
-	// pageContext.getRequest(),
-	// (HttpServletResponse) pageContext.getResponse());
-	// stagingConf.put(TableConfig.AJAX_SOURCE, processedUrl);
-	// this.dataSourceType = "AJAX";
-	// this.url = url;
-	// }
+      if (this.dynamicAttributes == null) {
+         this.dynamicAttributes = new HashMap<String, String>();
+      }
+
+      this.dynamicAttributes.put(localName, (String) value);
+   }
+
+   protected int setupHtmlGeneration() throws JspException {
+
+      JQueryContent datatableContent = new Select2JQueryContent(this.select);
+
+      // Get the existing JavaScript generator or create it if it doesn't exist
+      JQueryJsContentGenerator javascriptGenerator = (JQueryJsContentGenerator) AssetRequestContext.get(this.request)
+            .getGenerator(Select2Component.COMPONENT_NAME);
+
+      if (javascriptGenerator == null) {
+         javascriptGenerator = new JQueryJsContentGenerator(datatableContent);
+      }
+      else {
+         javascriptGenerator.appendContent(datatableContent);
+      }
+
+      // Update the asset request context with the enabled bundles and
+      // Javascript generator
+      AssetRequestContext.get(this.request).addBundles("ddl-s2").addGenerator(Select2Component.COMPONENT_NAME,
+            javascriptGenerator);
+
+      try {
+         this.pageContext.getOut().println(this.select.toHtml());
+
+      }
+      catch (IOException e) {
+         throw new JspException("Unable to generate the HTML markup for the table " + id, e);
+      }
+
+      return EVAL_PAGE;
+   }
+
+   public Map<Option<?>, Object> getStagingOptions() {
+      return stagingOptions;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public void setData(Collection<Object> data) {
+      this.dataSourceType = "DOM";
+      this.data = data;
+
+      Collection<Object> dataTmp = (Collection<Object>) data;
+      if (dataTmp != null && dataTmp.size() > 0) {
+         iterator = dataTmp.iterator();
+      }
+      else {
+         iterator = null;
+         currentObject = null;
+      }
+   }
+
+   public void setItemValue(String itemValue) {
+      this.itemValue = itemValue;
+   }
+
+   public void setItemLabel(String itemLabel) {
+      this.itemLabel = itemLabel;
+   }
+
+   /**
+    * <p>
+    * Validates the passed dynamic attribute.
+    * </p>
+    * <p>
+    * The dynamic attribute must not conflict with other attributes and must
+    * have a valid type.
+    * </p>
+    * 
+    * @param localName
+    *           Name of the dynamic attribute.
+    * @param value
+    *           Value of the dynamic attribute.
+    */
+   private void validateDynamicAttribute(String localName, Object value) {
+      if (!(value instanceof String)) {
+         throw new IllegalArgumentException(
+               "The attribute " + localName + " won't be added to the table. Only string values are accepted.");
+      }
+   }
+
+   public void setPlaceholder(String placeholder) {
+      stagingOptions.put(Select2Options.PLACEHOLDER, placeholder);
+   }
+
+   public void setAllowClear(Boolean allowClear) {
+      stagingOptions.put(Select2Options.ALLOW_CLEAR, allowClear);
+   }
+
+   public void setMultiple(Boolean multiple) {
+      stagingOptions.put(Select2Options.MULTIPLE, multiple);
+   }
+
+   public void setWidth(String width) {
+      stagingOptions.put(Select2Options.WIDTH, width);
+   }
+
+   public void setTags(Boolean tags) {
+      stagingOptions.put(Select2Options.TAGS, tags);
+   }
 }
